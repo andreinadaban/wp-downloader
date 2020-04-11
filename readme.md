d2e141a357086d0603e0d4879f763ba9b27d8c8d
Andrei Nadaban <contact@andreinadaban.com>
2020-04-11 12:34:13 +0300

    Script and instructions

---
+++ readme.md
@@ -0,0 +1,13 @@
+# WordPress Downloader
+
+A simple script used to download and install WordPress and the WordPress [starter theme](https://github.com/andreinadaban/wp-theme).
+
+## Installation
+
+1. Move the `wpd.sh` file into the `/usr/local/bin` directory
+2. Remove the `.sh` extension
+3. Make the file executable using the `chmod +x /usr/local/bin/wpd` command
+
+## Usage
+
+Run `wpd site-name site-title admin-user admin-password admin-email`.
---
+++ wpd.sh
@@ -0,0 +1,51 @@
+NAME=$1
+TITLE=$2
+USER=$3
+PASSWORD=$4
+EMAIL=$5
+
+PREFIX="$(openssl rand -hex 3)_"
+
+# Installation
+wp core download
+
+wp config create --dbname="${NAME}" --dbuser=root --dbpass=root --dbhost=127.0.0.1 --dbprefix="${PREFIX}"
+wp config set WP_DEBUG true --raw
+wp config set WP_DEBUG_DISPLAY true --raw
+wp config set WP_DEBUG_LOG true --raw
+
+wp db create
+
+wp core install --url="http://${NAME}.test" --title="${TITLE}" --admin_user="${USER}" --admin_password="${PASSWORD}" --admin_email="${EMAIL}" --skip-email
+
+wp option update blogdescription ''
+wp option update blog_public 0
+
+wp user meta update 1 show_admin_bar_front false
+wp user meta update 1 show_welcome_panel 0
+
+wp rewrite structure '/%postname%/'
+wp rewrite flush
+
+# Deletes all preinstalled plugins
+wp plugin delete --all
+
+# Deletes all preinstalled themes including the active one
+wp theme delete --all --force
+
+# Creates and activates the theme
+cd wp-content/themes
+
+# Downloads the starter theme
+git clone https://github.com/andreinadaban/wp-theme.git
+cd wp-theme
+rm -rf .git
+cd ../
+mv wp-theme "${NAME}"
+cd "${NAME}"
+rm readme.md
+touch readme.md
+
+cd ../../../
+
+wp theme activate "${NAME}"
